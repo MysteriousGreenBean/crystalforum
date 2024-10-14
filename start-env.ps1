70619ee13921ff8a7678c75f903a2b38cc22f5e3
Patryk Konieczny <pat366@gmail.com>
2024-10-14 16:10:19 +0200

    making templates rebuild on environment creation

--- start-env.ps1
+++ start-env.ps1
@@ -1,3 +1,7 @@
+Param(
+    [switch]$rebuildTemplates
+)
+
 # Define paths for your scripts
 $dockerComposeScript = "docker-compose up -d"
 $dockerComposeScript2 = "docker-compose stop"
@@ -13,6 +17,18 @@ while ($response.StatusCode -ne 200 -or ($response.Content -like "*Fatal error*"
     Start-Sleep -Seconds 1
     $response = Invoke-WebRequest -Uri $checkUrl
 }
+Write-Host "Database is ready."
+Write-Host "Building templates..."
+$templateRebuildUrl = "http://localhost/handlestylesheetsandtemplates.php?rebuild=templates&force=$rebuildTemplates"
+$response = Invoke-WebRequest -Uri $templateRebuildUrl
 
+if ($response.StatusCode -ne 200) {
+    Write-Host "Error building templates: " + $response.Content
+} 
+else
+{ 
+    Write-Host $response.Content
+    Write-Host "Templates are ready!"
+}
 # Start the background PowerShell script
 Invoke-Expression $backgroundScript
