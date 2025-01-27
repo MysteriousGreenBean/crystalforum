586f10bf961414a40db93af6fa088d6a99d1783d
Patryk Konieczny <pat366@gmail.com>
2025-01-27 20:19:43 +0100

    fixing curls and unzipping logic

--- website/handlezipfiles.php
+++ website/handlezipfiles.php
@@ -30,9 +30,38 @@ if (isset($_GET['unzip']) && isset($_GET['target'])) {
     if ($res === TRUE) {
         $zip->extractTo($targetDir);
         $zip->close();
-        echo 'Unzip successful';
+        echo 'Unzip successful<br>';
     } else {
-        echo 'Unzip failed';
+        echo 'Unzip failed<br>';
+    }
+
+    if ($_GET['target'] == 'current') {
+        // Handle moving contents of the 'current' folder to the current directory
+        $currentFolder = $currentDir . DIRECTORY_SEPARATOR . 'current' . DIRECTORY_SEPARATOR . basename($_GET['unzip'], '.zip');
+
+        if (is_dir($currentFolder)) {
+            // Open the 'current' folder and move its contents
+            $files = array_diff(scandir($currentFolder), array('..', '.')); // Get all files/folders except . and ..
+
+            foreach ($files as $file) {
+                $sourcePath = $currentFolder . DIRECTORY_SEPARATOR . $file;
+                $destPath = $currentDir . DIRECTORY_SEPARATOR . $file;
+
+                if (is_dir($sourcePath)) {
+                    // If it's a directory, use recursive move
+                    rename($sourcePath, $destPath); // Move directory
+                } else {
+                    // If it's a file, move it
+                    rename($sourcePath, $destPath); // Move file
+                }
+            }
+            echo 'Contents of the "current" folder have been moved.<br>';
+        } else {
+            echo 'Error: "current" folder does not exist.<br>';
+        }
+
+        // Remove the 'current' folder
+        rmdir($currentFolder);
     }
 } else {
     echo "Missing parameters.";
