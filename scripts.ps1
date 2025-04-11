795c6e63be2535e709fed103436e84b9dfda1fd0
MysteriousGreenBean <pat366@gmail.com>
2025-04-11 14:37:46 +0200

    78 dev manage database content (#82)
    * Allowing to manage database data (in some table)
    
    * updating dockerfile
    
    * adding purge containers method, hooking them up to launch and debug, fixing dockerfile that cause issues on newest image

--- scripts.ps1
+++ scripts.ps1
@@ -1,5 +1,14 @@
 function Start-Containers {
     docker-compose up -d
+    Wait-For-Containers
+    Database-Snapshot
+}
+
+function Wait-For-Containers {
+    do {
+        Start-Sleep -Seconds 1 
+        $unhealthy = docker ps --format "{{.Names}}: {{.Status}}" | Where-Object { $_ -match "unhealthy|starting" }
+    } while ($unhealthy)
 }
 
 function Stop-Containers {
@@ -11,20 +20,162 @@ function Restart-Containers {
     docker-compose up -d
 }
 
+function Purge-Containers {
+    docker-compose down --rmi all --volumes --remove-orphans
+}
+
 function View-Logs {
     docker-compose logs -f
 }
 
-function Liquibase-Update {
+function Database-Update {
     docker-compose run liquibase update
 }
 
-function Liquibase-Snapshot {
-    docker-compose run liquibase snapshot --snapshot-format=JSON --output-file=database_before.json
+function Database-Snapshot {
+    docker-compose run liquibase snapshot --snapshot-format=JSON --output-file=database_before.json --url=jdbc:mariadb://172.28.1.2:3306/crystalforum_ --username root --password root
+    $masterStatus = docker exec mybb_mariadb mariadb -uroot -proot -e "SHOW MASTER STATUS;"
+    $masterStatusArray = $masterStatus -split "`n" | Select-Object -Skip 1 | ForEach-Object {
+        $columns = $_ -split "`t"
+        [PSCustomObject]@{
+            File     = $columns[0]
+            Position = [int]$columns[1]
+        }
+    }
+    $masterStatusArray | ConvertTo-Json | Out-File -FilePath ./docker/liquibase/database_snapshot.json -Encoding utf8
 }
 
-function Liquibase-diffChangeLog {
+function Process-BinlogChanges {
+    param (
+        [string]$snapshotPosition,
+        [string]$snapshotFile,
+        [string]$monitoredTablesPattern,
+        [string]$inputPath,
+        [bool]$append
+    )
+
+    $binlogCommand = "mariadb-binlog --skip-gtid-strict-mode --start-position=$snapshotPosition --verbose --base64-output=DECODE-ROWS /var/lib/mysql/$snapshotFile > binlogDump.sql"
+    $binlogOutput = docker exec mybb_mariadb bash -c $binlogCommand
+
+    docker cp mybb_mariadb:binlogDump.sql ./docker/liquibase/binlogDump.sql
+
+    $content = Get-Content "./docker/liquibase/binlogDump.sql" -Raw
+
+    $delimiter = "/*!*/;"
+    $statements = $content -split [regex]::Escape($delimiter)
+
+    
+    $filtered = foreach ($stmt in $statements) {
+        if ($stmt -match $monitoredTablesPattern) {
+            # Trim whitespace and remove /*!*/; sequences
+            $cleaned = $stmt.Trim() -replace "`r?`n?", '' -replace '\s+', ' '
+            if ($cleaned -notmatch ";$") {
+                $cleaned += ";"
+            }
+            $cleaned
+        }
+    }
+    # Re-append the delimiter to each filtered statement
+    $finalContent = $filtered -join "`r`n"  -replace '/\*!.*?\*/', ''
+
+    # Append the final content to the file without overriding
+    if ($append) {
+        Add-Content -Path $inputPath -Value $finalContent
+    } else {
+        Set-Content -Path $inputPath -Value $finalContent
+    }
+
+    Remove-Item -Path "./docker/liquibase/binlogDump.sql"
+}
+
+function Create-Changeset {
+    param (
+        [string]$inputPath
+    )
+
+    $changelogFile = "./docker/liquibase/changelog/db.changelog.xml"
+    [xml]$xml = Get-Content -Path $changelogFile
+    $nsUri = $xml.DocumentElement.NamespaceURI
+
+    $generatedSQL = Get-Content -Path $inputPath -Raw
+    $generatedSQL = $generatedSQL.TrimEnd();
+
+    $changeSets = $xml.databaseChangeLog.changeSet
+    $lastChangeSet = $changeSets[-1]
+
+    if ([string]::IsNullOrWhiteSpace($generatedSQL)) {
+        Write-Host -ForegroundColor Yellow  "Generated SQL is empty. No new changeSet created."
+        return
+    }
+
+    if ($lastChangeSet.InnerText -eq $generatedSQL) {
+        Write-Host -ForegroundColor Yellow "Last changeSet is identical to the generated SQL. No new changeSet created."
+        return
+    }
+
+    $lastId = $lastChangeSet.id
+
+    $lastIdParts = $lastId -split '-'
+    $lastIdParts[0] = [int64]$lastIdParts[0] + 1
+    $lastIdParts[1] = "1"
+    $newId = $lastIdParts -join '-'
+
+    $changeSet = $xml.CreateElement("changeSet", $nsUri)
+    $changeSet.SetAttribute("id", $newId)
+    $author = git config user.name
+    $changeSet.SetAttribute("author", $author)
+    
+    $sqlNode = $xml.CreateElement("sql", $nsUri)
+    $sqlNode.InnerText = $generatedSQL.TrimEnd()
+    $changeSet.AppendChild($sqlNode) | Out-Null
+    $xml.databaseChangeLog.AppendChild($changeSet) | Out-Null
+
+    $xml.Save($changelogFile)
+}
+
+function Database-diffChangeLog {
     docker-compose run --rm liquibase liquibase diffChangeLog --url=offline:mariadb?snapshot=resources/database_before.json --referenceUrl=jdbc:mariadb://172.28.1.2:3306/crystalforum_ --referenceUsername root --referencePassword root --referenceDriver=org.mariadb.jdbc.Driver 
+
+    $masterStatus = docker exec mybb_mariadb mariadb -uroot -proot -e "SHOW MASTER STATUS;"
+    $masterStatusArray = $masterStatus -split "`n" | Select-Object -Skip 1 | ForEach-Object {
+        $columns = $_ -split "`t"
+        [PSCustomObject]@{
+            File     = $columns[0]
+            Position = [int]$columns[1]
+        }
+    }
+    $snapshot = Get-Content -Raw -Path ./docker/liquibase/database_snapshot.json | ConvertFrom-Json
+
+    $currentFile = $masterStatusArray[0].File
+    $currentPosition = $masterStatusArray[0].Position
+    $snapshotFile = $snapshot[0].File
+    $snapshotPosition = $snapshot[0].Position
+
+    $config = Get-Content -Raw -Path ./docker/liquibase/database_monitoring.config.json | ConvertFrom-Json
+
+    $monitoredTablesPattern = $config.monitoredTables -join "|"
+    $inputPath = "./docker/liquibase/changes.sql"
+
+    Process-BinlogChanges -snapshotPosition $snapshotPosition -snapshotFile $snapshotFile -monitoredTablesPattern $monitoredTablesPattern -inputPath $inputPath -append $false
+
+    if ($currentFile -ne $snapshotFile) {
+        $binaryLogs = docker exec mybb_mariadb mariadb -uroot -proot -e "SHOW BINARY LOGS;"
+        $binaryLogsArray = $binaryLogs -split "`n" | Select-Object -Skip 1 | ForEach-Object {
+            $columns = $_ -split "`t"
+            [PSCustomObject]@{
+                LogName = $columns[0]
+                FileSize = [int]$columns[1]
+            }
+        }
+
+        $logsToProcess = $binaryLogsArray | Where-Object { $_.LogName -gt $snapshotFile }
+
+        $logsToProcess | ForEach-Object { Write-Host $_.LogName }
+        foreach ($log in $logsToProcess) {
+            Process-BinlogChanges -snapshotPosition "0" -snapshotFile $log.LogName -monitoredTablesPattern $monitoredTablesPattern -inputPath $inputPath -append $true
+        }
+    } 
+    Create-Changeset -inputPath $inputPath
 }
 
 
@@ -32,9 +183,10 @@ switch ($args[0]) {
     "start" { Start-Containers }
     "stop" { Stop-Containers }
     "restart" { Restart-Containers }
+    "purge" { Purge-Containers }
     "logs" { View-Logs }
-    "liquibase_update" { Liquibase-Update }
-    "liquibase_snapshot" { Liquibase-Snapshot }
-    "createChangeLog" { Liquibase-diffChangeLog}
-    default { Write-Host "Usage: crystal {start|stop|restart|logs|liquibase_update|liquibase_snapshot|register}" }
+    "database-update" { Database-Update }
+    "database-snapshot" { Database-Snapshot }
+    "database-dumpChanges" { Database-diffChangeLog }
+    default { Write-Host "Usage: crystal {start|stop|restart|logs|database-update|database-snapshot|database-dumpChanges}" }
 }
