d164d778da7f7c0f51528b5eb8d64429c3442a84
Patryk Konieczny <pat366@gmail.com>
2025-05-20 20:26:29 +0200

    fix link

--- .devcontainer/initializeEnvironment.ps1
+++ .devcontainer/initializeEnvironment.ps1
@@ -1,6 +1,6 @@
 $settingsPath = Join-Path $PSScriptRoot '..\website\inc\settings.php'
 (Get-Content $settingsPath) -replace 'http:', 'https:' | Set-Content $settingsPath
-(Get-Content $settingsPath) -replace 'localhost', "$($ENV:CODESPACE_NAME)-80.github.dev" | Set-Content $settingsPath
+(Get-Content $settingsPath) -replace 'localhost', "$($ENV:CODESPACE_NAME)-80.app.github.dev" | Set-Content $settingsPath
 Write-Host "Starging the website..."
 & (Join-Path $PSScriptRoot '..\scripts.ps1') start
-Write-Host "Website started you can access it at https://$($ENV:CODESPACE_NAME)-80.github.dev"
+Write-Host "Website started you can access it at https://$($ENV:CODESPACE_NAME)-80.app.github.dev"
