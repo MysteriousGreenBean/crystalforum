3506640fe20f23120cfd54572115b5274361df41
Anna Podlasek <72233357+Irysek707@users.noreply.github.com>
2026-07-24 09:42:55 +0100

    added plugin for thread templates (#208)

---
+++ website/inc/languages/english/admin/config_defaultmessage.lang.php
@@ -0,0 +1,48 @@
+<?php
+
+/********************************************************************************************************************************
+*
+*  Default message (/inc/languages/english/admin/config_defaultmessage.lang.php)
+*  Author: Krzysztof "Supryk" Supryczyński
+*  Copyright: © 2013 - 2021 @ Krzysztof "Supryk" Supryczyński @ All rights reserved
+*  
+*  Website: http://supryk.eu/ 
+*  Description: Plugin allows the administrator to determine the default message , thread message.
+*
+********************************************************************************************************************************/
+/********************************************************************************************************************************
+*
+* This file is part of "Default message" plugin for MyBB.
+* Copyright © 2013 - 2021 @ Krzysztof "Supryk" Supryczyński @ All rights reserved
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+********************************************************************************************************************************/
+
+$l['defaultmessage'] = "Default message";
+$l['defaultmessage_desc'] = "Plugin allows the administrator to determine the default message , thread message, in forum.<br />If You like what i doing, support me on PayPal."; 
+
+$l['setting_group_defaultmessage'] = "Default message";
+$l['setting_group_defaultmessage_desc'] = "Settings for: Default message.";
+
+$l['setting_defaultmessage_onoff'] = "Enable plugin?";
+$l['setting_defaultmessage_onoff_desc'] = "This setting can tern on/off plugin.";
+
+$l['defaultmessage_uninstall'] = "Default message uninstallation";
+$l['defaultmessage_uninstall_message'] = "Do you wish to drop all data from the database?";
+
+$l['defaultmessage'] = "Default message";
+$l['defaultmessage_prefix_subject'] = "Default message subject.";
+$l['defaultmessage_prefix_thread'] = "Default message thread.";
+$l['defaultmessage_prefix_post'] = "Default message post.";
